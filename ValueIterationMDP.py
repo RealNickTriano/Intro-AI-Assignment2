9ab7e8fecb8cc860f6fceaa521b907c1af8b8a65
RealNickTriano <70707042+RealNickTriano@users.noreply.github.com>
2022-12-13 15:15:45 -0500

    changes to value iteration

--- ValueIterationMDP.py
+++ ValueIterationMDP.py
@@ -32,12 +32,11 @@ rewards=[0,0,1,0]
 
 gama=.1
 
-convergencediff=0.001
+convergencediff=0.00001
 
 stateVal=[0,0,1,0]
-
+print(stateVal)
 while True:
-    largestDiff=0
     vkplus1StateVal=[0,0,0,0]
     for i in range(len(states)):
         stateholder=states[i]
@@ -58,23 +57,28 @@ while True:
                         #taking max on it
                         #s1,a1,s1 .2   s1,a1,s2 .6
                         "s1->s2+s1->s1" "s1->s4+s1->s4"
-                        utility=gama*stateVal[k]*transitionProb[stateholder][actionholder][nextstateholder]
+                        utility = stateVal[k]*transitionProb[stateholder][actionholder][nextstateholder]
                         val1 += utility
-                bestaction[j] = val1
+                bestaction[j] = val1 * gama + rewards[i]
                 
 
 
         best = max(bestaction)
         print(best)
+        vkplus1StateVal[i] = best
         bestpolly=bestaction.index(best)
         print("for state" , states[i], "best policy is", actions[bestpolly])
         newReward =  best + rewards[i]
-        diff = abs(stateVal[i] - newReward)
+    
+    largestDiff = 0
+    for q in range(len(stateVal)):
+        diff = abs(stateVal[q] - vkplus1StateVal[q])
         largestDiff = max(largestDiff,diff)
-        stateVal[i] = newReward
-        print(stateVal)
-    if largestDiff<=convergencediff:
+    stateVal = vkplus1StateVal
+    print(stateVal)
+    if largestDiff <= convergencediff:
         break
+    
 print("the final utilities are",stateVal, "and the best policy is", )
 
 
